8c84b662d603a2a0b56d27697394b448d65e77e9
Mr.J <610076912@qq.com>
2018-12-06 23:03:26 +0800

    初始化

---
+++ src/CommentApp.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import CommentInput from 'CommentInput'
+import CommentList from 'CommentList'
+
+class CommentApp extends Component {
+
+}
+
+
+export default CommentApp
