8bf252f359d6f4b459449748b2f4d153e00795c2
Mr.Jxl <610076912@qq.com>
2018-12-07 18:27:26 +0800

    运行eject 实战1阶段完成

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react'
+import Comment from './Comment'
 
 class CommentList extends Component {
+	static defaultProps = {
+		comments: []
+	}
+
 	render() {
 		return (
-			<div>CommentList</div>
+			<div>
+				{this.props.comments.map((comment, index) => {
+					return (
+						<Comment comment={comment} key={index}/>
+					)
+				})}
+			</div>
 		)
 	}
 }
